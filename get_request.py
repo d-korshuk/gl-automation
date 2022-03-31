4f6d3c40d54bbbd0e40132d9aa21df8709b9ede4
Dmytro Korshuk <dmytro.korshuk@globallogic.com>
2022-03-31 10:24:45 +0300

    changed response variable name

--- get_request.py
+++ get_request.py
@@ -4,9 +4,9 @@ headers = {
         "User-Agent": "test"
 }
 
-response = requests.get("https://httpbin.org/get", headers=headers)
+r = requests.get("https://httpbin.org/get", headers=headers)
 
 
-print(response.raise_for_status())
-print(response.headers)
-print(response.url)
+print(r.raise_for_status())
+print(r.headers)
+print(r.url)
