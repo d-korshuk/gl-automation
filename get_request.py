8d760cb5a39c241be48636f70d3c92d55d9f6a3e
Dmytro Korshuk <dmytro.korshuk@globallogic.com>
2022-04-05 13:49:24 +0300

    added params to request

--- get_request.py
+++ get_request.py
@@ -1,11 +1,12 @@
 import requests
 
 base_url = "https://httpbin.org/get"
+payload = {"firstName" :"John", "lastName" :"Doe"}
 headers = {
         "User-Agent": "test"
 }
 
-r = requests.get(base_url, headers=headers)
+r = requests.get(base_url, headers=headers, params=payload)
 
 
 print(r.raise_for_status)
