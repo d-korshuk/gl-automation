f7fd4fb562c2745f2734f465dd5fa79a30fd3d0e
Dmytro Korshuk <dmytro.korshuk@globallogic.com>
2022-04-19 13:49:17 +0300

    removed token as a global variable

--- login_send_requests.py
+++ login_send_requests.py
@@ -1,7 +1,13 @@
 import requests
 import logging
-logging.basicConfig(filename="log.txt", level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s")
 
+
+
+
+#REMOVE GLOBAL AND ADD TOKEN AS PARAMETER
+
+#git commit "chnaged url names"
 
 
 login_url = "https://app.cosmosid.com/api/v1/login"
@@ -9,9 +15,9 @@ login_payload = {"expiry": 86400, "login_from": "login page"}
 login_headers = {"Authorization": "Basic bWFpbGludGVzdEB1a3IubmV0Om1haWxpbnRlc3RAdWtyLm5ldA=="}
 root_folder_url = "https://app.cosmosid.com/api/metagenid/v2/files?"
 files_count_url = "https://app.cosmosid.com/api/metagenid/v2/files/count?"
-get_runs_url = "https://app.cosmosid.com/api/metagenid/v1/files/7f4c7326-0a4e-4b56-a8d0-8ce002191672/runs?_=1622700773181"
-get_analyses_url = "https://app.cosmosid.com/api/metagenid/v1/runs/437ef8e4-b595-4fd8-a2f5-64221831e925/analysis?filter=total&_=1622700773184"
-get_artifacts_url = "https://app.cosmosid.com/api/metagenid/v1/runs/437ef8e4-b595-4fd8-a2f5-64221831e925/artifacts?_=1622700773185"
+runs_url = "https://app.cosmosid.com/api/metagenid/v1/files/7f4c7326-0a4e-4b56-a8d0-8ce002191672/runs?_=1622700773181"
+analyses_url = "https://app.cosmosid.com/api/metagenid/v1/runs/437ef8e4-b595-4fd8-a2f5-64221831e925/analysis?filter=total&_=1622700773184"
+artifacts_url = "https://app.cosmosid.com/api/metagenid/v1/runs/437ef8e4-b595-4fd8-a2f5-64221831e925/artifacts?_=1622700773185"
 
 root_folder_params = {"breadcrumbs": "1", "offset": "0", "limit": "1000", "folder_id": "84c966d5-8dce-429d-8f92-44d5e28b1581"}
 count_params = {"folder_id": "84c966d5-8dce-429d-8f92-44d5e28b1581"}
@@ -21,32 +27,36 @@ def login():
     r = requests.post(login_url, json=login_payload, headers=login_headers) 
     global access_token
     access_token = r.json()["token"]
-    logging.info(access_token)
-
+    return access_token
 
 def root_folder():
+    access_token = login()
     r = requests.get(root_folder_url, headers={"x-token": access_token}, params=root_folder_params)     
     logging.info(r.json())
 
 
 def files_count():
+    access_token = login()
     r = requests.get(files_count_url, headers={"x-token": access_token}, params=root_folder_params)       
-    logging.info(r.json())
+    logging.info(r.raise_for_status)
 
 
 def file_runs():
-    r = requests.get(get_runs_url, headers={"x-token": access_token})
-    logging.info(r.json())
+    access_token = login()
+    r = requests.get(runs_url, headers={"x-token": access_token})
+    logging.info(r.raise_for_status)
                                                                                                        
 
 def file_analyses():
-    r = requests.get(get_analyses_url, headers={"x-token": access_token})     
-    logging.info(r.json())
+    access_token = login()
+    r = requests.get(analyses_url, headers={"x-token": access_token})     
+    logging.info(r.raise_for_status)
                                                                                                  
 
-def file_artifacts():                                                                         
-    r = requests.get(get_artifacts_url, headers={"x-token": access_token}) 
-    logging.info(r.json())
+def file_artifacts():  
+    access_token = login()                                                                       
+    r = requests.get(artifacts_url, headers={"x-token": access_token}) 
+    logging.info(r.raise_for_status)
 
 
 if __name__ == "__main__":
